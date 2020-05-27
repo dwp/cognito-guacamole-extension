27e64f4c9443dec7a7319c92718f65b5e58c95ea
MattToczek <52243130+MattToczek@users.noreply.github.com>
2020-05-27 12:47:55 +0100

    reducing selfsigned cert. length to 30 days (#6)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-/usr/bin/openssl req -x509 -newkey rsa:4096 -keyout /usr/local/tomcat/conf/key.pem -out /usr/local/tomcat/conf/cert.pem -days 3650 -nodes -subj '/CN=guacamole'
+/usr/bin/openssl req -x509 -newkey rsa:4096 -keyout /usr/local/tomcat/conf/key.pem -out /usr/local/tomcat/conf/cert.pem -days 30 -nodes -subj '/CN=guacamole'
 
 /opt/guacamole/bin/start.sh $@
